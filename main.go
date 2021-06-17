77ddd501dc5085c17becaeff01bd7455b2f97ea0
pragnesh2435 <83198253+pragnesh2435@users.noreply.github.com>
2021-06-17 06:44:04 -0400

    Add files via upload

---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type counters struct {
+	sync.Mutex
+	view  int
+	click int
+}
+
+var (
+	c = counters{}
+
+	content = []string{"sports", "entertainment", "business", "education"}
+)
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to EQ Works 😎") // Corrected syntax
+
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	data := content[rand.Intn(len(content))]
+
+	c.Lock()
+	c.view++
+	c.Unlock()
+
+	err := processRequest(r)
+	if err != nil {
+		fmt.Println(err)
+		w.WriteHeader(400)
+		return
+	}
+
+	// simulate random click call
+	if rand.Intn(100) < 50 {
+		processClick(data)
+	}
+}
+
+func processRequest(r *http.Request) error {
+	time.Sleep(time.Duration(rand.Int31n(50)) * time.Millisecond)
+	return nil
+}
+
+func processClick(data string) error {
+	c.Lock()
+	c.click++
+	c.Unlock()
+
+	return nil
+}
+
+func statsHandler(w http.ResponseWriter, r *http.Request) {
+
+	n := time.Now() //Added time variable and used responsewriter to print on browser
+	fmt.Fprintf(w, "Sports : \n", n)
+	fmt.Fprintf(w, "Entertainement \n: ", n)
+	fmt.Fprintf(w, "Business : \n", n)
+	fmt.Fprintf(w, "Education : \n", n)
+
+	if !isAllowed() {
+		w.WriteHeader(429)
+		return
+	}
+}
+
+func isAllowed() bool {
+	return true
+}
+
+func uploadCounters() error {
+	return nil
+}
+
+func main() {
+
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/stats/", statsHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
